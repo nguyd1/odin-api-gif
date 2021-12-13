ff52832a9aaf2a972f93d824f56cb8ba2dc31ffc
nguyd1 <davennguy@gmail.com>
2021-12-13 12:58:56 -0800

    Add search bar

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const img=document.querySelector("img");
+const btn=document.querySelector("button");
+const input=document.querySelector("input");
 
-fetch("https://api.giphy.com/v1/gifs/translate?api_key=ytpfw643S4754fwpWwpwN8RONluD5T8x&s=cats",{mode:"cors"})
+let keyword="cats";
+
+function getImg(keyword){
+    fetch("https://api.giphy.com/v1/gifs/translate?api_key=ytpfw643S4754fwpWwpwN8RONluD5T8x&s="+keyword,{mode:"cors"})
     .then(function(response){
         return response.json();
     })
     .then(function(response){
         img.src=response.data.images.original.url;
     });
+}
+
+btn.addEventListener("click",()=>{
+    if(input.value) keyword=input.value;
+    getImg(keyword);
+})
+
+// init
+getImg(keyword);
