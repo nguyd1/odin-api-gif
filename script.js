96d4e7b993630d5f049559ff4bfc3e0b34bd3018
nguyd1 <davennguy@gmail.com>
2021-12-13 13:30:08 -0800

    Refactor to async await

--- script.js
+++ script.js
@@ -5,17 +5,15 @@ const p=document.querySelector("p");
 
 let keyword="cats";
 
-function getImg(keyword){
-    fetch("https://api.giphy.com/v1/gifs/translate?api_key=ytpfw643S4754fwpWwpwN8RONluD5T8x&s="+keyword,{mode:"cors"})
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(response){
-        img.src=response.data.images.original.url;
-    })
-    .catch(function(e){
+async function getImg(keyword){
+    try{
+        const response = await fetch("https://api.giphy.com/v1/gifs/translate?api_key=ytpfw643S4754fwpWwpwN8RONluD5T8x&s="+keyword,{mode:"cors"});
+        const data = await response.json();
+        img.src=data.data.images.original.url;
+    }
+    catch(e){
         p.textContent=e;
-    });
+    }
 }
 
 btn.addEventListener("click",()=>{
